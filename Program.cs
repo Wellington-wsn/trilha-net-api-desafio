5b4d0b885c22c6e0e9c26620b138f5c9e042d1a3
Wellington-wsn <fogosark@hotmail.com>
2023-01-30 20:25:25 -0300

    Finalizado desafio

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string Conexão = builder.Configuration.GetConnectionString("ConexaoPadrao");
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseMySql(builder.Configuration.GetConnectionString(Conexão), ServerVersion.AutoDetect(Conexão)));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
